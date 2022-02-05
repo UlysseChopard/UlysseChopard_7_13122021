d54cee9f9eba13bf669d23388f7e253c5f3e4387
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-02-05 12:36:03 +0000

    suppression des fichiers inutiles

--- back/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/auth");
-
-router.post("/login", controller.authenticate);
-router.post("/register", controller.create);
-
-module.exports = router;
