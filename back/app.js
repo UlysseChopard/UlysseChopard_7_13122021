03a52f6775f45d729a5634f62ee7cffc4b58bc00
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-03-07 10:48:06 +0000

    fix back async routes loading error

--- back/app.js
+++ back/app.js
@@ -5,36 +5,18 @@ const session = require("./session");
 const routes = require("./routes");
 const appMiddlewares = require("./middlewares/app");
 const cors = require("./middlewares/cors");
+const catchAll = require("./middlewares/catch_all");
 
 const { sequelize } = require("./models");
 
 const app = express();
 
-appMiddlewares(express, app);
-
 // Session avant CORS et routes
-session(sequelize, app);
-
-cors(app);
-
-routes(express, app);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error("Not found");
-  err.status = 404;
-  next(err);
+db().then((sequelize) => {
+  appMiddlewares(express, app);
+  session(sequelize, app);
+  cors(app);
+  routes(express, app);
+  catchAll(app);
+  return server(app);
 });
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: {},
-  });
-});
-
-db(sequelize);
-
-server(app);
